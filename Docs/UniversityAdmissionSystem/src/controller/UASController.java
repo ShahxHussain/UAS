86ab4eb9f225fcd8cd0e778addf175dbf5148b7e
Mahad Wajid <121817699+mahadwajid@users.noreply.github.com>
2023-06-06 21:33:40 +0500

    Mahad wajid

--- Docs/UniversityAdmissionSystem/src/controller/UASController.java
+++ Docs/UniversityAdmissionSystem/src/controller/UASController.java
@@ -8,6 +8,27 @@ package controller;
  *
  * @author S K
  */
+import java.util.Date;
+import java.sql.Time;
 public class UASController {
+    
+//Mahad Wajid (Fa21-bse-057 ) Use Case: Enter Personal Info 
+    public Student enterpersonalinfo(String firstname , String lastname , String email, String phoneno , String Address ){
+        
+        return Student;
+    }
+    
+    public Announcement createAnnouncement(String title , String content, String author ,Date date){
+        
+        return Announcement;
+    }
+    
+    public Schedule scheduletest(String testname, Date date, Time time ,String location){
+        return Schedule ;
+    }
+    
+//    ====================================================================
+    
+    
     
 }
