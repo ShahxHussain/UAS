0247ded74e9e1e2ee18a68bd35067b0668512f96
Hamza Badar <129106734+Hamza55-55@users.noreply.github.com>
2023-06-07 09:36:05 -0700

    hamza badar

--- Docs/UniversityAdmissionSystem/src/model/SQLConnection.java
+++ Docs/UniversityAdmissionSystem/src/model/SQLConnection.java
@@ -5,9 +5,41 @@
 package model;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SQLConnection {
-        public static String URL:"jdbc:mssql://localhost:3306/University Admission System";
-        public static String username:"sa";
-        public static String PASSWORD:"12345678";
-        
+    public static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=universityadmissionsystem";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "12345678";
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        try {
+            // Register the JDBC driver
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            
+            // Open a connection
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            
+            // Connection successful, perform database operations here
+            
+            // Close the connection
+            connection.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
+
